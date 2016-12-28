f03e9f92d22fc6975111ab3d3351278f7b0955f8
Eric Boxer <eboxhead@gmail.com>
2016-12-28 12:55:42 -0500

    Initial Commit

---
+++ BoxerStandardLibrary/BoxerStandardLibrary.h
@@ -0,0 +1,19 @@
+//
+//  BoxerStandardLibrary.h
+//  BoxerStandardLibrary
+//
+//  Created by Eric Boxer on 12/28/16.
+//  Copyright © 2016 Eric Boxer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BoxerStandardLibrary.
+FOUNDATION_EXPORT double BoxerStandardLibraryVersionNumber;
+
+//! Project version string for BoxerStandardLibrary.
+FOUNDATION_EXPORT const unsigned char BoxerStandardLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoxerStandardLibrary/PublicHeader.h>
+
+
