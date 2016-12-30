52b08e36bfdf0d924ae06b1aed1f285eae799432
Eric Boxer <eboxhead@gmail.com>
2016-12-29 21:23:02 -0500

    Initial Commit

---
+++ BoxerStandardLibrary/protocols.swift
@@ -0,0 +1,13 @@
+//
+//  protocols.swift
+//  BoxerStandardLibrary
+//
+//  Created by Eric Boxer on 12/28/16.
+//  Copyright © 2016 Eric Boxer. All rights reserved.
+//
+
+import Foundation
+
+protocol Passthrough {
+    func passtrhough()
+}
---
+++ BoxerStandardLibrary/types.swift
@@ -0,0 +1,15 @@
+//
+//  types.swift
+//  BoxerStandardLibrary
+//
+//  Created by Eric Boxer on 12/29/16.
+//  Copyright © 2016 Eric Boxer. All rights reserved.
+//
+
+import Foundation
+
+struct IPAddress {
+    
+    var ipAddress: String
+    
+}
